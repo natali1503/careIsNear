5c1aa7c5cab07407c886191e582004402f2103bc
natali1503 <nfv1503@gmail.com>
2024-11-21 18:12:46 +0500

    resolve problem with import

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 export default {
   testEnvironment: "jsdom", // Для тестирования DOM
+  testEnvironmentOptions: {
+    customExportConditions: ["node", "node-addons"],
+  },
   transform: {
     "^.+\\.vue$": "@vue/vue3-jest", // Для Vue
     "^.+\\.tsx?$": ["ts-jest", {}], // Для TypeScript
