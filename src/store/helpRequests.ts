d32097df286aa2fa8c509ed2ac799a3f455e6d3c
natali1503 <nfv1503@gmail.com>
2024-12-05 20:29:33 +0500

    add method getHelpRequests for helpRequests store

--- src/store/helpRequests.ts
+++ src/store/helpRequests.ts
@@ -1,11 +1,29 @@
-import { defineStore } from "pinia";
+import { defineStore } from 'pinia';
+import { getLocalStorage } from '../general/localStorage/getLocalStorage';
+import { api } from '../api/api';
+import { GetApiRequestResponse } from '../api/generated/types.gen';
 
-export const useHelpRequests = defineStore("helpRequests", {
+export const useHelpRequests = defineStore('helpRequests', {
   state: () => ({
     isLoading: false,
     isError: false,
-    data: [],
+    data: <GetApiRequestResponse>[],
   }),
   getters: {},
-  actions: {},
+  actions: {
+    async getHelpRequests() {
+      try {
+        if (this.data.length !== 0) return;
+        this.isLoading = true;
+        const token = getLocalStorage('tokenAuth');
+        if (token === null) return;
+        const data = await api.getRequests(token);
+        this.data = data;
+      } catch (e) {
+        this.isError = true;
+      } finally {
+        this.isLoading = false;
+      }
+    },
+  },
 });
