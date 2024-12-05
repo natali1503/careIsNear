5675fe3afa3432456101cf332151361b2c3158e1
natali1503 <nfv1503@gmail.com>
2024-12-05 20:49:59 +0500

    add test HelpRequests

--- tests/unit/Login.spec.ts
+++ tests/unit/Login.spec.ts
@@ -1,9 +1,9 @@
-import { mount } from "@vue/test-utils";
-import { createPinia, setActivePinia } from "pinia";
-import Login from "../../src/components/Login.vue";
-import { api } from "../../src/api/api";
+import { mount } from '@vue/test-utils';
+import { createPinia, setActivePinia } from 'pinia';
+import Login from '../../src/Pages/Login.vue';
+import { api } from '../../src/api/api';
 
-describe("Login", () => {
+describe('Login', () => {
   beforeEach(() => {
     // Создаём новое хранилище перед каждым тестом
     const pinia = createPinia();
@@ -12,77 +12,77 @@ describe("Login", () => {
     localStorage.clear();
   });
 
-  it("Наличие формы авторизации", () => {
+  it('Наличие формы авторизации', () => {
     const wrapper = mount(Login);
     expect(wrapper.find("input[placeholder='Логин']").exists()).toBe(true);
     expect(wrapper.find("input[placeholder='Пароль']").exists()).toBe(true);
-    expect(wrapper.find("button").text()).toBe("Войти");
+    expect(wrapper.find('button').text()).toBe('Войти');
   });
-  it("Обновление полей формы", async () => {
+  it('Обновление полей формы', async () => {
     const wrapper = mount(Login);
     const loginInput = wrapper.find("input[placeholder='Логин']");
     const passwordInput = wrapper.find("input[placeholder='Пароль']");
-    await loginInput.setValue("testLogin");
-    await passwordInput.setValue("testPassword");
+    await loginInput.setValue('testLogin');
+    await passwordInput.setValue('testPassword');
 
-    expect((loginInput.element as HTMLInputElement).value).toBe("testLogin");
-    expect((passwordInput.element as HTMLInputElement).value).toBe("testPassword");
+    expect((loginInput.element as HTMLInputElement).value).toBe('testLogin');
+    expect((passwordInput.element as HTMLInputElement).value).toBe('testPassword');
   });
   // it("Проверка вызова функции входа ", async () => {});
 
-  it("Должен сохранять токен в localStorage при авторизации", async () => {
+  it('Должен сохранять токен в localStorage при авторизации', async () => {
     //given
-    api.auth = jest.fn().mockResolvedValueOnce("wegwegwe");
+    api.auth = jest.fn().mockResolvedValueOnce('wegwegwe');
     const wrapper = mount(Login);
     const loginInput = wrapper.find("input[placeholder='Логин']");
     const passwordInput = wrapper.find("input[placeholder='Пароль']");
 
     //when
-    await loginInput.setValue("testLogin");
-    await passwordInput.setValue("testPassword");
-    await wrapper.find("button").trigger("click");
+    await loginInput.setValue('testLogin');
+    await passwordInput.setValue('testPassword');
+    await wrapper.find('button').trigger('click');
 
     //then
     expect(api.auth).toHaveBeenCalledTimes(1);
     expect(api.auth).toHaveBeenCalledWith({
-      login: "testLogin",
-      password: "testPassword",
+      login: 'testLogin',
+      password: 'testPassword',
     });
-    expect(localStorage.getItem("tokenAuth")).toBe("wegwegwe");
+    expect(localStorage.getItem('tokenAuth')).toBe('wegwegwe');
   });
-  it("Должен сохранять токен в localStorage при авторизации", async () => {
+  it('Должен сохранять токен в localStorage при авторизации', async () => {
     //given
-    api.auth = jest.fn().mockResolvedValueOnce("wegwegwe");
+    api.auth = jest.fn().mockResolvedValueOnce('wegwegwe');
     const wrapper = mount(Login);
     const loginInput = wrapper.find("input[placeholder='Логин']");
     const passwordInput = wrapper.find("input[placeholder='Пароль']");
 
     //when
-    await loginInput.setValue("testLogin");
-    await passwordInput.setValue("testPassword");
-    await wrapper.find("button").trigger("click");
+    await loginInput.setValue('testLogin');
+    await passwordInput.setValue('testPassword');
+    await wrapper.find('button').trigger('click');
 
     //then
     expect(api.auth).toHaveBeenCalledTimes(1);
     expect(api.auth).toHaveBeenCalledWith({
-      login: "testLogin",
-      password: "testPassword",
+      login: 'testLogin',
+      password: 'testPassword',
     });
-    expect(localStorage.getItem("tokenAuth")).toBe("wegwegwe");
+    expect(localStorage.getItem('tokenAuth')).toBe('wegwegwe');
   });
-  it("Обратка ошибки", async () => {
+  it('Обратка ошибки', async () => {
     //given
     api.auth = jest.fn().mockRejectedValueOnce(new Error());
     const wrapper = mount(Login);
     const loginInput = wrapper.find("input[placeholder='Логин']");
     const passwordInput = wrapper.find("input[placeholder='Пароль']");
     //when
-    await loginInput.setValue("testLogin");
-    await passwordInput.setValue("testPassword");
-    await wrapper.find("button").trigger("click");
+    await loginInput.setValue('testLogin');
+    await passwordInput.setValue('testPassword');
+    await wrapper.find('button').trigger('click');
     //then
     expect(api.auth).toHaveBeenCalledTimes(1);
-    const errorDiv = wrapper.find(".error");
+    const errorDiv = wrapper.find('.error');
     expect(errorDiv.exists()).toBe(true);
   });
 });
